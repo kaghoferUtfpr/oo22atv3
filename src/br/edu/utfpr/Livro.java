8a0ba1e94beee17cac44d6b05c41abc1999f3cc6
Daniel Kaghofer <kaghofer@alunos.utfpr.edu.br>
2022-05-13 17:48:21 -0300

    Criação de Classes

---
+++ src/br/edu/utfpr/Livro.java
@@ -0,0 +1,9 @@
+package br.edu.utfpr;
+
+public class Livro {
+
+    String autor;
+    String titulo;
+    int qtd;
+
+}
---
+++ src/br/edu/utfpr/Locacao.java
@@ -0,0 +1,12 @@
+package br.edu.utfpr;
+
+import java.util.Date;
+
+public class Locacao {
+
+    double valor;
+    Date dataLocacao;
+    Date dataDevolucao;
+    double multa;
+
+}
