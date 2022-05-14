3c9d71d02bdc9e748ebf3a5d93fe7319467c8f23
kaghofer <kaghofer@alunos.utfpr.edu.br>
2022-05-14 10:20:23 -0300

    Feito em casa

---
+++ src/br/edu/utfpr/Pessoa.java
@@ -0,0 +1,19 @@
+package br.edu.utfpr;
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+
+    public String getNome()
+    {
+        return this.nome;
+    }
+
+    public String getCpf()
+    {
+        return this.cpf;
+    }
+
+
+
+}
